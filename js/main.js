fee32f32f5fa0411f2868f9b660a2fae8a35d0d6
Valia <valia.vasileva1@gmail.com>
2020-10-22 18:00:47 +0300

    task1 finished

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var lenght=16;
+var lastName='Jhonson';
+
+var cars =["Saab", "Volvo", "BMW"];
+
+var person={
+    firstName: "John",
+    lastName: "Doe",
+}
+
+for(var i=0;i<cars.length;i++){
+    console.log(cars[i]);
+
+}
+
+for(var k in person){
+    console.log(person[k]);
+
+}
